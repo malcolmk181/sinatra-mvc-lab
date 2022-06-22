d506c20639eadba9ab1766fdbf1a51f9ea6cbbba
Malcolm Keyes <3.malcolmk@gmail.com>
2022-06-22 13:27:42 -0400

    commit tree 68e62e59150625466ae96e1e72a04b7937758c38

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,6 +4,21 @@ class PigLatinizer
     VOWELS = ['a','e','i','o','u','y']
 
     def piglatinize(input)
+        
+        # Turn input into an array of arrays
+        # Each array is an array of characters
+        # Each array contains either ONLY letters or ONLY non-letters
+        strings = input_to_arrays(input)
+
+        # Turn this array of arrays into the corrected arrays
+        arrays_to_piglatin_arrays!(strings)
+
+        # Flatten the arrays into one level and join into a string
+        strings.flatten.join
+    end
+
+    def input_to_arrays(input)
+
         chars = input.chars
         strings = []
 
@@ -31,48 +46,58 @@ class PigLatinizer
             end
         end
 
+        # handle a single non-consonant start creating
+        # an empty array in my array
         if strings.first == [] then strings.shift end
+          
+        # handle the fact that the last word will not be
+        # added to the string by the last character
         if word.count > 0 then strings << word end
 
         # strings is now an array of arrays
         # each array within strings is an array of characters
         # either all letters, or all non-letters
         strings
+    end
+    
+    # given an array of arrays, apply piglatin, and return string
+    def arrays_to_piglatin_arrays!(arrays)
+        arrays.each {|arr| piglatinize_word_array!(arr)}
 
-        # now I need to loop through the arrays
-        # if the array has non-letter characters in it
-        # I'll immediately concat it to a result array
-        # otherwise I'll do the pig latin, then append
-        # to result array
-        result = ""
+        arrays
+    end
 
-        for arr in strings do
-            if !LETTERS.include?(arr.first.downcase) then
-                # this is a non-letters array
-                result.concat(arr.join)
-            else
-                # this is an array of letters
-                if VOWELS.include?(arr.first.downcase) then
-                    # this is a vowel-started word
-                    # add way and send to result
-                    result.concat(arr.join + "way")
-                else
-                    # this is a consonant-started word
-                    # shovel shifted characters from the array
-                    # until the first char is a vowel
-                    # weird edge case to make sure there are vowels
-                    if VOWELS.any? {|vowel| arr.include?(vowel) } then
-                        until VOWELS.include?(arr.first.downcase) do
-                            arr << arr.shift
-                        end
-                    end
+    # given an array of characters, modify it to its piglatinized version
+    def piglatinize_word_array!(arr)
+        if VOWELS.include?(arr.first.downcase) then
+            # this is a vowel-started word
+            # just add way
+            arr << "way"
+        elsif LETTERS.include?(arr.first.downcase) then
+            # this is a consonant-started word
 
-                    result.concat(arr.join + "ay")
+            # shovel shifted characters from the array
+            # until the first char is a vowel
+
+            # be sure to check there is at least one vowel in this word!
+            if VOWELS.any? {|vowel| arr.include?(vowel) } then
+                until VOWELS.include?(arr.first.downcase) do
+                    arr << arr.shift
                 end
             end
-        end
-        result
 
-        # this is broken! turns into an infinite loop for the word "My"
+            arr << "ay"
+        end
+        # else, this is not a word of letters, so no modification necessary
     end
+
+
 end
+
+# gets all non character strings (although adds a "" to the beginning if starts with a non-letter):
+# "asdf as f DDDd   f f  ... . .d asdf".split(/[a-z,A-Z]+/)
+
+# gets all the character strings:
+# "asdf as f DDDd   f f  ... . .d asdf".split(/[^a-z,A-Z]+/)
+
+# so for recombining, would literally just recombine starting with non-character strings and alternate
